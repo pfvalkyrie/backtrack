2aa9e4de562ea1013b9f8dd35aabdd54a280f830
apollo13th <100813183+pfvalkyrie@users.noreply.github.com>
2026-05-16 04:55:41 -0700

    Initial Commit

---
+++ Sidetrack/SidetrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidetrackApp.swift
+//  Sidetrack
+//
+//  Created by Ulises on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SidetrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
